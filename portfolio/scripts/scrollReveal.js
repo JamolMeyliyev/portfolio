dca2ba67a215d6e5c5c7b553a502851206fffeb3
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-31 19:23:00 +0500

    Add project files.

---
+++ portfolio/scripts/scrollReveal.js
@@ -0,0 +1,10 @@
+﻿export default function initScrollReveal(targetElements, defaultProps) {
+    if (!targetElements.length) return;
+
+    ScrollReveal({ reset: false });
+
+    targetElements.forEach(({ element, animation }) => {
+        ScrollReveal().reveal(element, Object.assign({}, defaultProps, animation));
+    });
+}
+
---
+++ portfolio/scripts/tiltAnimation.js
@@ -0,0 +1,6 @@
+﻿import VanillaTilt from "vanilla-tilt";
+
+export default function initTiltAnimation() {
+    const elements = document.querySelectorAll(".js-tilt");
+    VanillaTilt.init(elements);
+}
